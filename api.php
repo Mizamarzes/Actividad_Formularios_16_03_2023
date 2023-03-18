3427bda3ab5065faadd14333deaa6890b5e77644
JuanContreras <mizamarzes@gmail.com>
2023-03-18 11:00:58 -0500

    formulario con servidor

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 <?php 
     header('Access-Control-Allow-Origin: *');
     $_DATA = json_decode(file_get_contents("php://input"), true);
-
+    $_DATA["servidor"] = $_SERVER["HTTP_HOST"];
     echo json_encode($_DATA,JSON_PRETTY_PRINT);
 ?>
